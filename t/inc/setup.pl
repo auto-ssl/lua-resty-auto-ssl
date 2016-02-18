f440253556874bda4d2d05d722c9a1b317c834c9
Nick Muerdter <nick.muerdter@nrel.gov>
2016-02-18 16:22:18 -0700

    Add comment explaining why we retain the private account key for tests.

--- t/inc/setup.pl
+++ t/inc/setup.pl
@@ -11,9 +11,13 @@ $ngrok->expect(10, "-re", "Hostname:([a-z0-9]+.ngrok.io)") or die "failed to fin
 $ENV{TEST_NGINX_NGROK_HOSTNAME} = ($ngrok->matchlist())[0] or die "failed to extract hostname for ngrok";
 
 $ENV{TEST_NGINX_RESTY_AUTO_SSL_DIR} = "/tmp/resty-auto-ssl-test";
+
+# If the tests have previously been run, wipe out any test data, but ensure we
+# retain the private account key registered with Let's Encrypt. If we don't,
+# Let's Encrypt's staging environment still has limits on this initial account
+# registration, so you can end up blocked due to repeated registrations.
 my $letsencrypt_key = read_file("$ENV{TEST_NGINX_RESTY_AUTO_SSL_DIR}/letsencrypt/private_key.pem", err_mode => "quiet");
 remove_tree($ENV{TEST_NGINX_RESTY_AUTO_SSL_DIR});
-
 if($letsencrypt_key) {
   make_path("$ENV{TEST_NGINX_RESTY_AUTO_SSL_DIR}/letsencrypt");
   write_file("$ENV{TEST_NGINX_RESTY_AUTO_SSL_DIR}/letsencrypt/private_key.pem", $letsencrypt_key);
